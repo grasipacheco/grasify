b3d5932ea992df8077e8ab24574c10d4c40b8ec8
Grasiele Pacheco <grasiele2191@gmail.com>
2023-12-04 17:32:32 +0100

    correct app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from "react";
-import { data } from "./db";
+import { data } from "./db.js";
 import AlbumCard from "./components/AlbumCard";
+import "";
 
 export default function App() {
-  console.log("its not working");
   return (
-    <div className="app">
-      <h1 className="app-header">Grasify</h1>
-      <ul>
-        {data.map((info) => (
-          <li key={info.id}>
-            <AlbumCard
-              title={info.title}
-              artist={info.artist}
-              albumCover={info.image.url}
-            />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className="app">
+        <h1 className="app-header">Grasify</h1>
+        <ul className="app-albums">
+          console.log(data)
+          {data.map((info) => (
+            <li key={info.id}>
+              <AlbumCard
+                title={info.title}
+                author={info.author}
+                albumCover={info.image.url}
+              ></AlbumCard>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
