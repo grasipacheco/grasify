863884b19200d25996566b6431bf0cc9075a8a07
Grasiele Pacheco <grasiele2191@gmail.com>
2023-12-07 16:43:54 +0100

    add AlbumList component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,22 +3,20 @@ import "./AlbumList.css";
 
 export default function AlbumList({ title, albums }) {
   return (
-    <>
-      <section>
-        <h2>{title}</h2>
-        <ul className="app-albums">
-          {albums.map((album) => (
-            <li key={album.id}>
-              <AlbumCard
-                title={album.title}
-                artist={album.artist}
-                albumCover={album.image.url}
-                songs={album.tracks}
-              />
-            </li>
-          ))}
-        </ul>
-      </section>
-    </>
+    <section>
+      <h2>{title}</h2>
+      <ul className="app-albums">
+        {albums.map((album) => (
+          <li key={album.id}>
+            <AlbumCard
+              title={album.title}
+              artist={album.artist}
+              albumCover={album.image.url}
+              songs={album.tracks}
+            />
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }
